6f7cfc9beb4d577833d648a54709737c6850fb18
ElitestarsSam <62185235+ElitestarsSam@users.noreply.github.com>
2021-02-12 10:24:54 +0000

    Commit 2

--- src/main/java/tech/bedev/api/API.java
+++ src/main/java/tech/bedev/api/API.java
@@ -4,20 +4,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class API extends JavaPlugin {
+public class API {
 
     public String msg(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
     public void addCmd(String cmd, CommandExecutor executor) {
-        this.getCommand(cmd).setExecutor(executor);
+        Bukkit.getPluginCommand(cmd).setExecutor(executor);
     }
 
     public void addTab(String cmd, TabCompleter completer) {
-        this.getCommand(cmd).setTabCompleter(completer);
+        Bukkit.getPluginCommand(cmd).setTabCompleter(completer);
     }
 
     public void startup(String pluginName, String enablingDisabling, String discordUrl, String authors, String version, String pluginUrl, String wikiUrl) {
